96e87c7b80ea0674964654d7d2424d231729264f
Erjan <tasmatoverzan05@gmail.com>
2025-02-24 18:10:12 +0600

    Изменил на новый обработчик

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -1,13 +1,13 @@
 package kg.attractor.java;
 
-import kg.attractor.java.lesson44.Lesson44Server;
+import kg.attractor.java.lesson44.Handler;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            new Lesson44Server("localhost", 9889).start();
+            new Handler("localhost", 9889).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
